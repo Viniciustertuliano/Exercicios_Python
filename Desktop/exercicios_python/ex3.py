a8aeb4d7b5a6bfa07d1a2f84a7393b96d2d2eae4
Viniciustertuliano <41974347+Viniciustertuliano@users.noreply.github.com>
2019-09-15 08:10:25 -0300

    Exercicio 3 que pede para converter temperatura de Celsius para Fahrenheit

---
+++ Desktop/exercicios_python/ex3.py
@@ -0,0 +1,7 @@
+#Modifique o fluxograma de conversão de temperatura, construído anteriormente, para
+#converter de Celsius para Fahrenheit. A fórmula de conversão é: F = C • 9/5 + 32
+
+c = int (input("Digite a temperatura em Celsius: "))
+
+
+print("o valor da temperatura em Fahrenheit é: ", c * 9/5 + 32)
